c8f5f418c7be0dfe49e75d1221a0ef7982470215
cornelious Tarwireyi <cornelioustarwireyi@gmail.com>
2022-12-10 10:22:55 +0200

    spelling fix

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,9 +3,9 @@ public class HelloWorld {
     public static void main(String[] args) {
      Car renault = new  Car (1000,"White");
      Car merc = new  Car (1200,"Blue");
-     System.out.println("Renault without load weighs " + renault.getWeight());
+     System.out.println("Renault without load weights " + renault.getWeight());
      renault.setWeight(3000);
-     System.out.println("Renault with load weighs " + renault.getWeight());
+     System.out.println("Renault with load weights " + renault.getWeight());
      System.out.println(merc.getColor());
      renault.drive();
      renault.brake();
